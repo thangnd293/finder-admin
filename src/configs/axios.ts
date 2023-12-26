777e5b1300c009234c63873b6ca5853797b0792d
Nguyen Dac Thang [thangnd] <thangnd@nexondv.com>
2023-12-26 16:43:58 +0700

    chore: setup env

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -2,7 +2,7 @@ import { eraseCookie, getCookie } from "@/utils/cookie";
 import axios, { InternalAxiosRequestConfig } from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `https://finder.sohe.in/api/v1`,
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   timeout: 5000,
   headers: { Accept: "application/json", "Content-Type": "application/json" },
 });
